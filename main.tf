dd6ecd1572496c755fdd0d00db9288184ec96651
Mark Menger <51541068+mjmenger@users.noreply.github.com>
2021-12-01 15:29:27 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -348,8 +348,8 @@ resource "azurerm_network_interface" "mgmt_nic" {
   ip_configuration {
     name                          = "${local.instance_prefix}-mgmt-ip-${count.index}"
     subnet_id                     = local.bigip_map["mgmt_subnet_ids"][count.index]["subnet_id"]
-    private_ip_address_allocation = (length(local.mgmt_public_private_ip_primary[count.index]) > 0 ? "Static" : "Dynamic")
-    private_ip_address            = (length(local.mgmt_public_private_ip_primary[count.index]) > 0 ? local.mgmt_public_private_ip_primary[count.index] : null)
+    private_ip_address_allocation = length(local.mgmt_public_private_ip_primary) > 0 && (length(local.mgmt_public_private_ip_primary[count.index]) > 0 ? "Static" : "Dynamic")
+    private_ip_address            = length(local.mgmt_public_private_ip_primary) > 0 && (length(local.mgmt_public_private_ip_primary[count.index]) > 0 ? local.mgmt_public_private_ip_primary[count.index] : null)
     public_ip_address_id          = local.bigip_map["mgmt_subnet_ids"][count.index]["public_ip"] ? azurerm_public_ip.mgmt_public_ip[count.index].id : ""
   }
   tags = merge(local.tags, {
