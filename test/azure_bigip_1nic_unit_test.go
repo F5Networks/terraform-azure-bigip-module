79f9323f32f0f0ed568875df54aef3837697cfa1
RavinderReddyF5 <r.chinthalapalli@f5.com>
2022-02-26 02:37:18 +0530

    adding test fix

--- test/azure_bigip_1nic_unit_test.go
+++ test/azure_bigip_1nic_unit_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTerraformAzure1NicExample(t *testing.T) {
@@ -12,8 +13,8 @@ func TestTerraformAzure1NicExample(t *testing.T) {
 
 	terraformOptions := &terraform.Options{
 
-		TerraformDir: "../example/bigip_azure_1nic_deploy",
-
+		TerraformDir: "../examples/bigip_azure_1nic_deploy",
+		
 		Vars: map[string]interface{}{
 			"location": "eastus",
 		},
@@ -23,4 +24,16 @@ func TestTerraformAzure1NicExample(t *testing.T) {
 
 	terraform.InitAndApply(t, terraformOptions)
 
+	mgmtPublicIP := terraform.Output(t, terraformOptions, "mgmtPublicIP")
+	bigipPassword := terraform.Output(t, terraformOptions, "bigip_password")
+	bigipUsername := terraform.Output(t, terraformOptions, "bigip_username")
+	mgmtPort := terraform.Output(t, terraformOptions, "mgmtPort")
+	mgmtPublicURL := terraform.Output(t, terraformOptions, "mgmtPublicURL")
+
+	assert.NotEqual(t, "", mgmtPublicIP[0])
+	assert.NotEqual(t, "", bigipPassword[0])
+	assert.NotEqual(t, "", bigipUsername[0])
+	assert.Equal(t, "8443", mgmtPort[0])
+	assert.NotEqual(t, "", mgmtPublicURL[0])
+	
 }
--- test/azure_bigip_3nic_unit_test.go
+++ test/azure_bigip_3nic_unit_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTerraformAzure3NicExample(t *testing.T) {
@@ -12,7 +13,7 @@ func TestTerraformAzure3NicExample(t *testing.T) {
 
 	terraformOptions := &terraform.Options{
 
-		TerraformDir: "../example/bigip_azure_3nic_deploy",
+		TerraformDir: "../examples/bigip_azure_3nic_deploy",
 
 		Vars: map[string]interface{}{
 			"location": "eastus",
@@ -22,5 +23,17 @@ func TestTerraformAzure3NicExample(t *testing.T) {
 	defer terraform.Destroy(t, terraformOptions)
 
 	terraform.InitAndApply(t, terraformOptions)
+
+	mgmtPublicIP := terraform.Output(t, terraformOptions, "mgmtPublicIP")
+	bigipPassword := terraform.Output(t, terraformOptions, "bigip_password")
+	bigipUsername := terraform.Output(t, terraformOptions, "bigip_username")
+	mgmtPort := terraform.Output(t, terraformOptions, "mgmtPort")
+	mgmtPublicURL := terraform.Output(t, terraformOptions, "mgmtPublicURL")
+
+	assert.NotEqual(t, "", mgmtPublicIP[0])
+	assert.NotEqual(t, "", bigipPassword[0])
+	assert.NotEqual(t, "", bigipUsername[0])
+	assert.Equal(t, "443", mgmtPort[0])
+	assert.NotEqual(t, "", mgmtPublicURL[0])
 
 }
