61e946604e58e65e74730ecce9d09187480fa4d4
RavinderReddyF5 <r.chinthalapalli@f5.com>
2022-03-03 17:24:19 +0530

    fixing test issue

--- test/azure_bigip_1nic_unit_test.go
+++ test/azure_bigip_1nic_unit_test.go
@@ -1,8 +1,13 @@
 package test
 
 import (
+	"crypto/tls"
+	"fmt"
 	"testing"
+	"time"
 
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
@@ -30,10 +35,38 @@ func TestTerraformAzure1NicExample(t *testing.T) {
 	mgmtPort := terraform.Output(t, terraformOptions, "mgmtPort")
 	mgmtPublicURL := terraform.Output(t, terraformOptions, "mgmtPublicURL")
 
-	assert.NotEqual(t, "", mgmtPublicIP[0])
-	assert.NotEqual(t, "", bigipPassword[0])
-	assert.NotEqual(t, "", bigipUsername[0])
-	assert.Equal(t, "8443", mgmtPort[0])
-	assert.NotEqual(t, "", mgmtPublicURL[0])
-	
+	mgmtPublicIP = mgmtPublicIP[1 : len(mgmtPublicIP)-1]
+	bigipPassword = bigipPassword[1 : len(bigipPassword)-1]
+	bigipUsername = bigipUsername[1 : len(bigipUsername)-1]
+	mgmtPort = mgmtPort[1 : len(mgmtPort)-1]
+	mgmtPublicURL = mgmtPublicURL[1 : len(mgmtPublicURL)-1]
+
+	assert.NotEqual(t, "", mgmtPublicIP)
+	assert.NotEqual(t, "", bigipPassword)
+	assert.NotEqual(t, "", bigipUsername)
+	assert.Equal(t, "8443", mgmtPort)
+	assert.NotEqual(t, "", mgmtPublicURL)
+
+	logger.Logf(t, "mgmtPublicURL:%+v",mgmtPublicURL)
+	logger.Logf(t, "bigipPassword:%+v",bigipPassword)
+
+	testUrl := fmt.Sprintf("https://%s:%s@%s:%s/mgmt/shared/appsvcs/info",bigipUsername,bigipPassword,mgmtPublicIP,mgmtPort)
+
+	logger.Logf(t, "testUrl:%+v",testUrl)
+
+
+	// fmt.Sprintf("https://%s:%s@%s:%s/mgmt/shared/appsvcs/info", bigipUsername[0], bigipPassword[0], mgmtPublicIP[0], mgmtPort[0])
+
+	tlsConfig := tls.Config{}
+
+	http_helper.HttpGetWithRetryWithCustomValidation(
+		t,
+		testUrl,
+		&tlsConfig,
+		10,
+		10*time.Second,
+		func(statusCode int, body string) bool {
+			return statusCode == 200
+		},
+	)
 }
