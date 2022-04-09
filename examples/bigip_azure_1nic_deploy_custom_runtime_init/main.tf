2f95f60029989927f4a3446ec841deb508d97fa9
RavinderReddyF5 <r.chinthalapalli@f5.com>
2022-04-09 23:55:31 +0530

    fixing azurerm 3.0 issues

--- examples/bigip_azure_1nic_deploy_custom_runtime_init/main.tf
+++ examples/bigip_azure_1nic_deploy_custom_runtime_init/main.tf
@@ -51,7 +51,7 @@ module "bigip" {
   f5_ssh_publickey            = azurerm_ssh_public_key.f5_key.public_key
   mgmt_subnet_ids             = [{ "subnet_id" = data.azurerm_subnet.mgmt.id, "public_ip" = true, "private_ip_primary" = "" }]
   mgmt_securitygroup_ids      = [module.mgmt-network-security-group.network_security_group_id]
-  availabilityZones           = var.availabilityZones
+  availability_zone           = var.availability_zone
   availabilityZones_public_ip = var.availabilityZones_public_ip
   custom_user_data            = data.template_file.user_data_vm0.rendered
 }
--- examples/bigip_azure_1nic_deploy_module_count/terraform.tfvars
+++ examples/bigip_azure_1nic_deploy_module_count/terraform.tfvars
@@ -1,5 +1,5 @@
 # BIG-IP Environment
 location          = "eastus"
-instance_count    = 2
+instance_count    = 1
 AllowedIPs        = ["0.0.0.0/0"]
 availabilityZones = ["1"]
